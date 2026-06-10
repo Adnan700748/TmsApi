bbdaf4919f9e3bc97e0e573f0b71547939b56498
Owner <adenejib07@gmail.com>
2026-06-10 23:33:54 +0300

    feat(api): add environment-specific api surface

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35602b7151b2bebad32682d4c783144c24512096")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d73e5e3fbb28317ed447b48d22d55a78ec073bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
