83b171040817378f243fa6c18fe80ba6b790cd94
Adnan700748 <adenejib07@gmail.com>
2026-06-13 11:07:23 +0300

    some changes

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d06ba62aee2611d00e355084211c06730819023")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faaadaad46779c0abf9bce3bb53f137e8cab101d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
