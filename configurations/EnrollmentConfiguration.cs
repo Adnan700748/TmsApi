d7496ba5993d46640d6c0bb5817a8575d4b49a24
Adnan700748 <adenejib07@gmail.com>
2026-06-20 12:06:01 +0300

    feat(config):add entity configurations using Fluent API

---
+++ configurations/EnrollmentConfiguration.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TmsApi.Entities;
+
+namespace TmsApi.Configurations;
+
+public class EnrollmentConfiguration : IEntityTypeConfiguration<Enrollment>
+{
+    public void Configure(EntityTypeBuilder<Enrollment> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.StudentId)
+            .IsRequired();
+
+        builder.Property(e => e.CourseId)
+            .IsRequired();
+
+        builder.HasOne(e => e.Student)
+            .WithMany(s => s.Enrollments)
+            .HasForeignKey(e => e.StudentId);
+
+        builder.HasOne(e => e.Course)
+            .WithMany(c => c.Enrollments)
+            .HasForeignKey(e => e.CourseId);
+    }
+}
