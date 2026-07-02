f71dcbea1f6439b70a1c7f102df2080b5a8ff52a
Adnan700748 <adenejib07@gmail.com>
2026-07-02 23:10:46 +0300

    feat(Dtos): adjustment on request Dtos

--- Dtos/CreateCourseRequest.cs
+++ Dtos/CreateCourseRequest.cs
@@ -1,13 +1,10 @@
 using System.ComponentModel.DataAnnotations;
-
 namespace TmsApi.Dtos;
 
 public record CreateCourseRequest
 {
     [Required]
-    [RegularExpression(
-        @"^[A-Z]{3}-\d{3}$",
-        ErrorMessage = "Code must follow the pattern XXX-000 (e.g., CSE-101).")]
+    [RegularExpression( @"^[A-Z]{3}-\d{3}$", ErrorMessage = "Code must follow the pattern XXX-000 (e.g., CSE-101).")]
     public required string Code { get; init; }
 
     [Required]
