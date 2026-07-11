295c2213a3e0b380e07f7cb6fa81532020f48d22
Adnan700748 <adenejib07@gamil.com>
2026-07-11 11:05:44 +0300

    Add DTO contracts for student API

---
+++ Services/IStudentService.cs
@@ -0,0 +1,11 @@
+using TmsApi.Dtos;
+
+public interface IStudentService
+{
+    Task<StudentResponseDto> AddAsync(CreateStudentRequest request);
+    Task<StudentResponseDto?> GetByIdAsync(string id);
+    Task<IReadOnlyList<StudentResponseDto>> GetAllAsync();
+    Task<bool> DeleteAsync(string id);
+    Task<StudentResponseDto?> UpdateAsync(int id, UpdateStudentRequest request);
+    Task<StudentResponseDto?> GetByIdAsync(int id, CancellationToken ct);
+}
