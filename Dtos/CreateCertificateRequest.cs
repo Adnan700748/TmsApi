c7e447d45da2939a0a864d081f8994638b1063bf
Adnan700748 <adenejib07@gmail.com>
2026-07-13 20:45:56 +0300

    Add certificate request and response DTOs

---
+++ Dtos/CreateCertificateRequest.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TmsApi.Dtos;
+
+public record CreateCertificateRequest
+{
+    [Required]
+    public required string SerialNumber { get; init; }
+
+    [Range(1, int.MaxValue)]
+    public required int StudentId { get; init; }
+
+    [Range(1, int.MaxValue)]
+    public required int CourseId { get; init; }
+}
