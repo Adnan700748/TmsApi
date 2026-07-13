95a8bec61821ccb7803bc54364b8bf4507161ce5
Adnan700748 <adenejib07@gmail.com>
2026-07-13 23:52:38 +0300

    feat(assessments): add assessment DTOs

---
+++ Dtos/CreateAssessmentRequest.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TmsApi.Dtos;
+
+public record CreateAssessmentRequest
+{
+    [Required]
+    [StringLength(200)]
+    public required string Title { get; init; }
+
+    [Range(1, 1000)]
+    public decimal MaxScore { get; init; }
+
+    [Range(0.01, 1)]
+    public decimal Weight { get; init; }
+
+    [Range(1, int.MaxValue)]
+    public int CourseId { get; init; }
+}
